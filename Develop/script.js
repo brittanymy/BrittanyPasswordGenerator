fa540af903b6e543590d6622c414402013bf9918
brittanymy <brittany.young2001@gmail.com>
2022-03-03 16:50:07 -0500

    updated comments in js

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,25 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword(thisIsThePasswordLength) {
+  console.log("Button Clicked!") // Confirmation button was clicked
+  
+  // Give user prompts for password
+    // Set character length between 8-128
+  
+
+
+
+    // Can include lowercase, uppercase, numeric or special characters
+  
+  // Validated input - At least 1 character type selected
+
+  // Generate password for user based off of prompts
+ 
+  // Display generated password on page
+  return "Password will go here!" // 
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
